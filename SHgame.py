1dd8a91889fea9f86072b0783a74f10ef7e4c006
KK2NJ <kk2boss007@gmail.com>
2025-09-13 16:45:11 -0400

    added changes to main and SHgame

--- SHgame.py
+++ SHgame.py
@@ -1,8 +1,23 @@
 import numpy as np
 from elections import Elections
-
+from fascist_board import FascistBoard
+from liberal_board import LiberalBoard
+from roles import Roles
+from pick_government import PickGovernment
+from deck import Deck
 
 class SecretHitler_3P:
     def __init__(self, num_players=3):
         self.num_players = num_players
         self.elections = Elections()
+        self.fascist_board = FascistBoard()
+        self.liberal_board = LiberalBoard()
+        self.roles = Roles(num_players)
+        self.pick_government = PickGovernment()
+        self.deck = Deck()
+    
+    def start(self):
+        print("")
+        self.roles.assign_roles()
+        self.elections.begin()
+        
---
+++ main.py
@@ -0,0 +1,6 @@
+from SHgame import SecretHitler_3P
+
+if __name__ == "__main__":
+    game = SecretHitler_3P(num_players=3)
+    print("Game initialized with 3 players.")
+    game.start()
